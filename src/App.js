d0b77c27dc7d7af5a490cba92da88f46b9eb45bf
Mario <mamerida2013@gmail.com>
2021-12-28 22:27:49 -0300

    generando la logica para calcular el interes compuesto y asingnandola al boton de subMit

--- src/App.js
+++ src/App.js
@@ -17,9 +17,23 @@ const Section = styled.section`
   width: 500px;
   box-shadow:0px 2px 3px rgb(0,0,0,0.3)
 `
+// creo la funcion que calcula mi total
+const compoundInterets = (deposito,contribution,years,rate) =>{
+  //variable que va a contener el dinero total
+  let total = deposito
+  for( let i=0; i < years ; i++){
+      total = ( total + contribution ) * ( rate + 1)
 
-const handleSubmit = () =>{
+  }
+  return Math.round(total);
 
+}
+
+
+const handleSubmit = ({deposit,contribution,years,rate}) =>{
+  //como los valores vienen como texto tengo que castearlo a number
+    const val = compoundInterets(Number(deposit),Number(contribution),Number(years),Number(rate))
+    console.log(val)
 }
 function App() {
 
